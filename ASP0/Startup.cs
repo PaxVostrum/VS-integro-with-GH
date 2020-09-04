c3e339544bb04f4aa9f585b39499bb338f7c4ebc
rh <rh@mail.com>
2020-09-04 10:33:28 +0300

    Added some bigass text yay!

--- ASP0/Startup.cs
+++ ASP0/Startup.cs
@@ -27,7 +27,7 @@ namespace ASP0
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync($"Hello World! \nTHIS IS SOMETHING I'VE ADDED");
             });
         }
     }
